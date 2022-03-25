2de01297eaf49e1bface524c6ec2b8f1979923e7
Carlos <cadudrop@hotmail.com>
2022-03-25 14:04:10 -0300

    adiciona src

---
+++ src/main/java/br/com/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,8 @@
+package br.com.biblioteca;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class BibliotecaApplication extends Application {
+}
